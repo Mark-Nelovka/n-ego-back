5cb606c0f8ff67fdf3638f1229339475f9326278
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-02-27 02:08:11 +0200

    add: tslint.json and change path

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-
-import express from "express";
-// import express from "express";
+import express from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
 import loginRouter from "./routers/login";
 import newsRouter from "./routers/news";
-import cors from "cors";
-import dotenv from "dotenv"
+
+// const app = express();
+// const port = 3000;
+// app.get('/', (req, res) => {
+//   res.send('The sedulous hyena ate the antelope!');
+// });
+// app.listen(port, err => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   return console.log(`server is listening on ${port}`);
+// });
+
+// const express = require("express")
+// const loginRouter = require("./routers/login") 
+// const newsRouter = require("./routers/news");
+// const cors = require("cors") 
+// const dotenv = require("dotenv") 
 
 dotenv.config()
 
@@ -17,12 +33,12 @@ app.use("/login", loginRouter);
 app.use("/news", newsRouter);
 
 
-app.use((req: express.Request, res: express.Response) => {
+app.use((req, res) => {
   res.status(404).json({ message: "404 Not found" });
 });
 
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.status(500).json({ message: err.message });
+app.use((req, res) => {
+  res.status(500).json({ message: res.statusMessage });
 });
 
 app.listen(4040, () => {
