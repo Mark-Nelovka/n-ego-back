f5e05d3caafdc69b6587e525207775714beab8fc
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-02-27 13:16:35 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import cors from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 import loginRouter from "./routers/login";
 import newsRouter from "./routers/news";
@@ -7,7 +7,7 @@ import newsRouter from "./routers/news";
 dotenv.config()
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/login", loginRouter);
